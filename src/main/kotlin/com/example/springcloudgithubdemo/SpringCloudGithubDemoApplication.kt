a1e921df00d86a1dc8cf2fbd7860270595a81a50
isk-bokang <bo-kang@iskra.world>
2024-09-01 20:41:08 +0900

    initial commit

---
+++ src/main/kotlin/com/example/springcloudgithubdemo/SpringCloudGithubDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springcloudgithubdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudGithubDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCloudGithubDemoApplication>(*args)
+}
