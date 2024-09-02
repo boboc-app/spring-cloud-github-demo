2656bfeadfdea73105b6c50ac2b8f42f81350a74
isk-bokang <bo-kang@iskra.world>
2024-09-02 21:38:46 +0900

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.3.3"
+	id("org.springframework.boot") version "3.1.9"
 	id("io.spring.dependency-management") version "1.1.6"
 }
 
@@ -10,7 +10,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
 
@@ -26,7 +26,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	implementation("app.boboc:spring-cloud-github:0.0.1")
+	implementation("app.boboc:spring-cloud-github:0.0.2")
 }
 
 kotlin {
--- src/main/kotlin/com/example/springcloudgithubdemo/SpringCloudGithubDemoApplication.kt
+++ src/main/kotlin/com/example/springcloudgithubdemo/SpringCloudGithubDemoApplication.kt
@@ -1,11 +1,29 @@
 package com.example.springcloudgithubdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 
+@EnableConfigurationProperties(DemoProperties::class)
 @SpringBootApplication
 class SpringCloudGithubDemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<SpringCloudGithubDemoApplication>(*args)
 }
+
+@Configuration
+class PropTestConfiguration(
+	private val demoProperties: DemoProperties
+){
+	init {
+	    println(demoProperties.myMessage)
+	}
+}
+
+@ConfigurationProperties("props")
+data class DemoProperties(
+	val myMessage: String
+)
